6e717a25847fbe8e369435337f58ec7820a4fe1d
Boo Zhan Yi <boozhanyi@gmail.com>
2023-10-05 15:26:02 +0800

    commit

--- app/page.js
+++ app/page.js
@@ -35,7 +35,7 @@ export default async function Home({ searchParams }) {
           <section>
             <div className="home__cars-wrapper">
               {allCars.map((car) => (
-                <CarCard car={car} />
+                <CarCard car={car} key={car.id} />
               ))}
             </div>
             <ShowMore
